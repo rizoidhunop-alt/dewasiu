4d6df8d5106082c7a8ef628d25c866268748e135
rizoidhunop-alt <rizoidhunop@gmail.com>
2025-09-02 14:01:27 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "rizoidhunop.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "dewasiuio"; // Ganti dengan nama workers kalian
+const serviceName = "nuutree"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
